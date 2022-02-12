0d60dfcfa5a9444cc0225d46b258f54163602005
김지나 <jinagold.k@gmail.com>
2022-02-12 14:57:21 +0900

    init: 프로젝트 세팅

---
+++ DodleDodle/DodleDodle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DodleDodle
+//
+//  Created by 김지나 on 2022/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
