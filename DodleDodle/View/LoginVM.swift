2ea87fdaba254186207bbc9f90c108f480f3e757
김지나 <jinagold.k@gmail.com>
2022-02-19 15:10:17 +0900

    feat: [BASKET-25] 로그인 기능 구현

--- DodleDodle/View/LoginVM.swift
+++ DodleDodle/View/LoginVM.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class LoginVM: NSObject {
+    var user: User!
+    var email: String { return user.email }
+    var password: String { return user.password }
+    
+    typealias loginAuthCallBack = (_ email: String, _ password: String) -> Void
+    var callBack: loginAuthCallBack?
+    
+    private func authenticateLogin() {
+        
+    }
+    
+    
+}
